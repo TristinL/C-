11aca5daaee10c256a60f4159f8fa45b5c9a0410
Tristin Lehman <44233586+TristinL@users.noreply.github.com>
2019-01-31 15:44:42 -0500

    Fix Comments

--- Colored_text.cpp
+++ Colored_text.cpp
@@ -6,16 +6,19 @@
 using namespace std;
 
 int main(){
-	//create a handle to the computer screen.
-	HANDLE screen = GetStdHandle(STD_OUTPUT_HANDLE);
 	
-	//write 16 lines in 16 different colors
-	for (int color = 0; color < 16; color++){
+	HANDLE screen = GetStdHandle(STD_OUTPUT_HANDLE);	//create a handle to the computer screen.
+	
+	for (int color = 0; color < 16; color++){		//write 16 lines in 16 different colors
+		
 		SetConsoleTextAttribute(screen, color);
+
 		cout<< "Hello World!"<<endl;
-		Sleep(400);		//pause between lines to see them appear
+		
+		Sleep(400);					//pause between lines to see them appear
 	}
-	//Restore the normal text color
-	SetConsoleTextAttribute(screen, 7);
+	
+	SetConsoleTextAttribute(screen, 7);			//Restore the normal text color
+	
 	return 0;
 }
