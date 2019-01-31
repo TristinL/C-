c0f2e361864daa39e8bedbd4ce2dc50c3ba6f3b0
Tristin Lehman <44233586+TristinL@users.noreply.github.com>
2019-01-31 16:18:56 -0500

    Fixed Comments

--- Constants.h
+++ Constants.h
@@ -1,14 +1,15 @@
-//	constants header file
+//	Constants header file for Constant_bouncing_ball.cpp
 
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
  
-// define your own namespace to hold constants
-namespace myConstants
-{
-    const double pi(3.14159);
-    const double avogadro(6.0221413e23);
-    const double my_gravity(9.8); // m/s^2 -- gravity is normal on this planet
-    // ... other related constants
+namespace myConstants{                     // define your own namespace to hold constants
+ 
+    const double pi(3.14159);              //simple definition for pi to 5 places
+ 
+    const double avogadro(6.0221413e23);   //definition for avogadro's number
+ 
+    const double my_gravity(9.8);          // m/s^2 -- gravity is normal on this planet
+                                           // ... other related constants that were unused, but space remains for them
 }
 #endif
