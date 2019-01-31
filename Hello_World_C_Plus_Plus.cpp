fd21b2aa2060406d187af814bca31082c71a3aab
Tristin Lehman <44233586+TristinL@users.noreply.github.com>
2019-01-31 17:34:19 -0500

    Fixed comments

--- Hello_World_C_Plus_Plus.cpp
+++ Hello_World_C_Plus_Plus.cpp
@@ -1,10 +1,8 @@
+//This is my hello world! program. The first one I did in C++. It is here for sentimental reasons as others in other languages are.
+
 #include <iostream>
 
 int main(){
+	
 	std::cout << "Hello world!" << std::endl;
-	
-	
-	int x;
-	x = 5;
-	std::cout << x;
 }
